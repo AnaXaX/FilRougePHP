3bf7ec88502ba2e2be9e71f8eef5c63ba241ca24
AnaXaX <anas.boudyach@gmail.com>
2018-03-27 20:39:38 +0200

    panix
    qsd

--- panier.php
+++ panier.php
@@ -8,10 +8,9 @@ require_once 'php/Helper.php';
             <table id="cart" class="table table-hover table-condensed">
                 <thead>
                     <tr>
-                        <th style="width:50%">Product</th>
-                        <th style="width:10%">Price</th>
-                        <th style="width:8%">Quantity</th>
-                        <th style="width:22%" class="text-center">Subtotal</th>
+                        <th style="width:50%">Entrée</th>
+                        <th style="width:10%">Prix</th>
+                        <th style="width:30%" class="text-center">Date entrée</th>
                         <th style="width:10%"></th>
                     </tr>
                 </thead>
@@ -19,18 +18,16 @@ require_once 'php/Helper.php';
                     <tr>
                         <td data-th="Product">
                             <div class="row">
-                                <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive" /></div>
+                              
                                 <div class="col-sm-10">
-                                    <h4 class="nomargin">Product 1</h4>
-                                    <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
+                                    <h4 class="nomargin">numEntrée ...</h4>
+                                    <p>nom Visiteur .. infors entrée visit</p>
                                 </div>
                             </div>
                         </td>
-                        <td data-th="Price">$1.99</td>
-                        <td data-th="Quantity">
-                            <input type="number" class="form-control text-center" value="1">
-                        </td>
-                        <td data-th="Subtotal" class="text-center">1.99</td>
+                        <td data-th="Price">get prix entrée</td>
+                       
+                        <td data-th="date" class="text-center">1995-10-1</td>
                         <td class="actions" data-th="">
                             <button class="btn btn-info btn-sm"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></button>
                             <button class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
@@ -42,10 +39,10 @@ require_once 'php/Helper.php';
                         <td class="text-center"><strong>Total 1.99</strong></td>
                     </tr>
                     <tr>
-                        <td><a href="#" class="btn btn-warning"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Continue Shopping</a></td>
+                        <td><a href="#" class="btn btn-warning"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>Continuer la visite</a></td>
                         <td colspan="2" class="hidden-xs"></td>
                         <td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
-                        <td><a href="#" class="btn btn-success btn-block">Checkout <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a></td>
+                        <td><a href="validationAchat.php" class="btn btn-success btn-block">Payement <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a></td>
                     </tr>
                 </tfoot>
             </table>
