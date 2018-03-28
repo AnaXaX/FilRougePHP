d062aecfcbd17a52152a0fd25ca29974ef221b1d
AnaXaX <anas.boudyach@gmail.com>
2018-03-28 20:19:20 +0200

    tar
    tar

--- tarifs.php
+++ tarifs.php
@@ -10,28 +10,29 @@ $page = 2;
 
 <div class="container">
     <div class="jumbotron MinOpacity">
-        <h2><b> Découvrez nos offres ! </b></h2>
+     <center><h2><b> Découvrez nos offres ! </b></h2>
         <p><i> Entrées classiques à 10 euros, 5 euros pour les mineurs (-18 ans) </i></p>
         <p><a class="btn btn-primary btn-large" href="FormEntreeClassique.php">Achetez vos entrées !</a></p>
+        </center>
     </div>
-	<div class="row">
+    	<div class="row">
     			<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
 				
 					<!-- PRICE ITEM -->
 					<div class="panel price panel-red">
 						<div class="panel-heading  text-center">
-						<h3>PRO PLAN</h3>
+						<h3>ENTREE ACTIVITE : Nagez avec les Dauphins !</h3>
 						</div>
 						<div class="panel-body text-center">
-							<p class="lead" style="font-size:40px"><strong>$10 / month</strong></p>
+							<p class="lead" style="font-size:40px"><strong> 25 € </strong></p>
 						</div>
 						<ul class="list-group list-group-flush text-center">
-							<li class="list-group-item"><i class="icon-ok text-danger"></i> Personal use</li>
-							<li class="list-group-item"><i class="icon-ok text-danger"></i> Unlimited projects</li>
-							<li class="list-group-item"><i class="icon-ok text-danger"></i> 27/7 support</li>
+							<li class="list-group-item"><i class="icon-ok text-danger"></i> <b> Visite du Zoo </b> </li>
+							<li class="list-group-item"><i class="icon-ok text-danger"></i> <b>Découverte du bassin aquatique et des dauphins </b> </li>
+							<li class="list-group-item"><i class="icon-ok text-danger"></i> <b> Baignade & Contact avec les Dauphins (30min) </b></li>
 						</ul>
 						<div class="panel-footer">
-							<a class="btn btn-lg btn-block btn-danger" href="#">BUY NOW!</a>
+							<a class="btn btn-lg btn-block btn-danger" href="#">RESERVEZ MAINTENANT !</a>
 						</div>
 					</div>
 					<!-- /PRICE ITEM -->
@@ -44,10 +45,83 @@ $page = 2;
 					<!-- PRICE ITEM -->
 					<div class="panel price panel-blue">
 						<div class="panel-heading arrow_box text-center">
+						<h3>ENTREE ACTIVITE : Escape Game by Zoo Parc </h3>
+						</div>
+						<div class="panel-body text-center">
+							<p class="lead" style="font-size:40px"><strong> 20 € </strong></p>
+						</div>
+						<ul class="list-group list-group-flush text-center">
+							<li class="list-group-item"><i class="icon-ok text-info"></i><b> Visite du Zoo </b></li>
+							<li class="list-group-item"><i class="icon-ok text-info"></i><b> Résolvez les énigmes laissez par le doyen pour sortir de la salle !</b></li>
+							<li class="list-group-item"><i class="icon-ok text-info"></i><b> Boissons & Goûter offert si sortie dans le temps imparti </b></li>
+						</ul>
+						<div class="panel-footer">
+							<a class="btn btn-lg btn-block btn-info" href="#">RESERVEZ MAINTENANT !</a>
+						</div>
+					</div>
+					<!-- /PRICE ITEM -->
+					
+					
+				</div>
+				
+				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+				
+					<!-- PRICE ITEM -->
+					<div class="panel price panel-green">
+						<div class="panel-heading arrow_box text-center">
+						<h3> ENTREE SPECTACLE : Envolez-vous ! </h3>
+						</div>
+						<div class="panel-body text-center">
+							<p class="lead" style="font-size:40px"><strong> 15 € </strong></p>
+						</div>
+						<ul class="list-group list-group-flush text-center">
+							<li class="list-group-item"><i class="icon-ok text-success"></i><b> Visite du Zoo </b></li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i><b> Entrée pour le Spectacle Aérien du Zoo </b></li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i><b> Accès Backstage (Supplément de 10€) </b></li>
+						</ul>
+						<div class="panel-footer">
+							<a class="btn btn-lg btn-block btn-success" href="#">RESERVEZ MAINTENANT </a>
+						</div>
+					</div>
+					<!-- /PRICE ITEM -->
+					
+					
+				</div>
+				
+				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+				
+					<!-- PRICE ITEM -->
+					<div class="panel price panel-grey">
+						<div class="panel-heading arrow_box text-center">
+						<h3> ENTREE SPECTACLE :20000 lieues sous le Zoo </h3>
+						</div>
+						<div class="panel-body text-center">
+							<p class="lead" style="font-size:40px"><strong> 15 € </strong></p>
+						</div>
+						<ul class="list-group list-group-flush text-center">
+							<li class="list-group-item"><i class="icon-ok text-success"></i><b> Visite du Zoo </b></li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i><b> Entrée pour le Spectacle Aquatique du Zoo </b></li>
+							<li class="list-group-item"><i class="icon-ok text-success"></i><b> Accès Backstage (Supplément de 10€) </b></li>
+							
+						</ul>
+						<div class="panel-footer">
+							<a class="btn btn-lg btn-block btn-primary" href="#">RESERVEZ MAINTENANT !</a>
+						</div>
+					</div>
+					<!-- /PRICE ITEM -->
+            </div>
+    </div>
+	<div class="row">
+				
+				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+				
+					<!-- PRICE ITEM -->
+					<div class="panel price panel-blue">
+						<div class="panel-heading arrow_box text-center">
 						<h3>DEV PLAN</h3>
 						</div>
 						<div class="panel-body text-center">
-							<p class="lead" style="font-size:40px"><strong>$20 / month</strong></p>
+							<p class="lead" style="font-size:40px"><strong>20€ / month</strong></p>
 						</div>
 						<ul class="list-group list-group-flush text-center">
 							<li class="list-group-item"><i class="icon-ok text-info"></i> Personal use</li>
@@ -62,7 +136,7 @@ $page = 2;
 					
 					
 				</div>
-				
+            
 				<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
 				
 					<!-- PRICE ITEM -->
